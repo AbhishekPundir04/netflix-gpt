4246e82bc3a78d6a29453cfb54c846839cee41a5
AbhishekPundir04 <pundirabhishek471@gmail.com>
2024-06-12 14:04:46 +0530

    code updated

--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -12,6 +12,7 @@ const VideoBackground = ({movieId}) => {
   return (
     <div className=' '>  
       <iframe className='w-screen aspect-video' src={`https://www.youtube.com/embed/XeDbyVODQ5M?si=${nowMoviesTrailer?.key} &autoplay=1&mute=1`} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowFullScreen></iframe>
+      {/* <iframe width="560" height="315" src="https://www.youtube.com/embed/5ci48K8vITY?si=6eWi4ULUoYlGjjE2&autoplay=1&mute=1" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> */}
       </div>
   )
 }
